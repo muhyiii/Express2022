7ae4a8fa6a6b43a05fd84c532235c176f170bf86
muhyiii <muhyiii47@gmail.com>
2022-01-12 11:19:28 +0700

    express 2022

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT ||5000
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Selamat Datang')
+})
+
+
+
+
+////////////////////////////////////////////////////////////////////
+app.listen(port, () => {
+  console.log(`Server Berjalan di port ${port}`);
+});
+////////////////////////////////////////////////////////////////////
