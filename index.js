1de308298812f8b0f47ec9452792d0804fd35e4a
muhyiii <muhyiii47@gmail.com>
2022-01-12 14:34:27 +0700

    tes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv')
 dotenv.config()
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
 
 
