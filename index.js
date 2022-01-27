5c1298cdfbf609aae9bb7ebc9bfbffd2baeab0ba
muhyiii <muhyiii47@gmail.com>
2022-01-27 08:31:12 +0700

    Migrate

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv')
-dotenv.config()
-const port = process.env.PORT || 5000
-const router = require('./routes/index')
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 5000;
+const router = require("./routes/index");
+const cors = require("cors");
 
-
-
-app.use(express.json())
-app.use(router)
-
-
-
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 ////////////////////////////////////////////////////////////////////
 app.listen(port, () => {
